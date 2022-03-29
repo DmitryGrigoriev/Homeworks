6a5d3b5355c9a53d84d5a6d4b4a85fa2bc35de14
DmitryGrigoriev <grigd2013@gmail.com>
2022-03-30 01:11:07 +0500

    Add args change

--- homework_01/main.py
+++ homework_01/main.py
@@ -11,7 +11,7 @@ def power_numbers(*args):
     >>> power_numbers(1, 2, 5, 7)
     [1, 4, 25, 49]
     """
-    return [number ** 2 for number in [*args]]
+    return [number ** 2 for number in args]
 
 
 # filter types
