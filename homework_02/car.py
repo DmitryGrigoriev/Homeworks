2cd968d7168c66a5287a1f8d7d3cc8a1990a854e
DmitryGrigoriev <grigd2013@gmail.com>
2022-05-02 11:02:06 +0500

    Make engine worked

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,19 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+
+
+class Car(Vehicle):
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
+
+    def set_engine(self, engine):
+        self.engine = engine
+
+
+car = Car(50, 100, 200)
+v = Vehicle()
+print(isinstance(car, Vehicle))
